8133c6a6ec41088b9fc806b41a0bcaf2680926bc
Muhammad Farooq <fm6182218@gmail.com>
2026-02-07 18:47:35 +0500

    One More Time

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 import User from "./Module/userModule.js";
 
 mongoose
@@ -20,20 +21,40 @@ app.post("/register", async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        // validation
         if (!name || !email || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
 
+        const hashPassword = await bcrypt.hash(password, 10);
+
         const user = await User.create({
             name,
             email,
-            password,
+            password: hashPassword,
         });
 
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ error: error.message });
+    }
+});
+
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body || {};
+
+    const [user] = await User.find({ email });
+
+    if (!user) {
+        return res.status(400).send("User not found!");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+        res.json(user);
+    } else {
+        res.status(400).json({ message: "Password not match!" });
     }
 });
 
